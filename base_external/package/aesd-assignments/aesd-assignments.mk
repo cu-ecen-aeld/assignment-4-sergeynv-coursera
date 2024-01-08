c796abce99f9d55591d8cf3702d79d8a308beb0f
Sergey Nikolaienkov <sergeynv@google.com>
2024-01-08 15:44:47 +0100

    [a4-p2] link 'assignments-3-and-later-sergeynv-coursera' repo

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,12 +5,10 @@
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+# 'assignments-3-and-later-sergeynv-coursera' repo, 'assignment-4-part-1' tag/release
+# https://github.com/cu-ecen-aeld/assignments-3-and-later-sergeynv-coursera/releases/tag/assignment-4-part-1
+AESD_ASSIGNMENTS_VERSION = 'e807d915ae30619b36bd44ffbd6d38644f568bc4'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-sergeynv-coursera.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
