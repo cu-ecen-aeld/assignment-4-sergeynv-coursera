0bca8adb77293685f7eca4c2a9f9ff53139aee52
Sergey Nikolaienkov <sergeynv@google.com>
2024-01-09 17:17:31 +0100

    [a4-p2] aesd-assignments.mk: install finder-app; update AESD_ASSIGNMENTS_VERSION

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,22 +6,36 @@
 ##############################################################
 
 # Repo:    assignments-3-and-later-sergeynv-coursera
-# Commit:  [a4-p2] finder-test.sh: write output to /tmp/assignmentX-result.txt
-# https://github.com/cu-ecen-aeld/assignments-3-and-later-sergeynv-coursera/commit/7ff398383c24a27ff463040fc8017e2ca8afd251
-AESD_ASSIGNMENTS_VERSION = '7ff398383c24a27ff463040fc8017e2ca8afd251'
+# Commit:  envsetup.sh: set AELD_ASSIGNMENTS_DIR
+# https://github.com/cu-ecen-aeld/assignments-3-and-later-sergeynv-coursera/commit/da2885dcd61f74e377554afd1cc4b9280475dd7b
+AESD_ASSIGNMENTS_VERSION = 'da2885dcd61f74e377554afd1cc4b9280475dd7b'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-sergeynv-coursera.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
+# May want/need to run the following first:
+# sudo ln -sf $(realpath "${AESD_ASSIGNMENTS_DIR}/..") /aeld
+# AESD_ASSIGNMENTS_OVERRIDE_SRCDIR = '/aeld/assignments/'
+
+# Really need to 'make clean'?
+# (E.g. in case we are using _OVERRIDE_SRCDIR where we build with a different toolchain?)
 define AESD_ASSIGNMENTS_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app clean
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+
+# Add writer, finder and finder-test utilities/scripts.
+# TODO(sergeynv): writer aso needs "/lib/ld-linux-aarch64.so.1" and "libc.so.6" which are currently missing!
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755   $(@D)/finder-app/finder.sh       $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755   $(@D)/finder-app/finder-test.sh  $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755   $(@D)/finder-app/writer          $(TARGET_DIR)/usr/bin
+
+	$(INSTALL) -d 0755   $(@D)/conf/   $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755   $(@D)/conf/*  $(TARGET_DIR)/etc/finder-app/conf/
+
+	$(INSTALL) -m 0755   $(@D)/assignment-autotest/test/assignment4/*  $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
